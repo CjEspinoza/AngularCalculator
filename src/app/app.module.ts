c4b15fc5bf8b559ed30eb8ec4aaaebf5baac9529
ProjectGuardian <espinoza.cj3@gmail.com>
2021-03-03 13:04:00 +0800

    finishing touches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BodyComponent } from './components/body/body.component';
 import { CalcComponent } from './components/calc/calc.component';
-import { InputComponent } from './components/input/input.component';
+import { TipsComponent } from './components/tips/tips.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     BodyComponent,
     CalcComponent,
-    InputComponent
+    TipsComponent
+
   ],
   imports: [
     BrowserModule,
